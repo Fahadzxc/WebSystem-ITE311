d3ba8766009372ca544d23dffdc1f3aa7375b68b
Fahadzxc <fahadalalawi1815@gmail.com>
2025-08-07 13:07:30 +0800

    Initial commit - CodeIgniter setup

--- public/index.php
+++ public/index.php
@@ -1,7 +1,10 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
 use CodeIgniter\Boot;
 use Config\Paths;
+define('CI_ENVIRONMENT', 'development');
 
 /*
  *---------------------------------------------------------------
